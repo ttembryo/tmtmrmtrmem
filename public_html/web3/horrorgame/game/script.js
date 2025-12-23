8517b2a220ccc5b07ff0b2808867b1b7719c8d27
4nnisbored <niod351@gmail.com>
2025-12-23 07:49:41 +0000

    build 1

--- public_html/web3/horrorgame/game/script.js
+++ public_html/web3/horrorgame/game/script.js
@@ -6,10 +6,17 @@ const view = document.getElementById("view")
 const shudder = document.getElementById("picture")
 // const statue = document.getElementById("statue")
 const dialogue = document.getElementById("dialogue")
-let step1 = new Audio("audio/walk1.m4a")
-let step2 = new Audio("audio/walk2.m4a")
-let step3 = new Audio("audio/walk3.m4a")
-let step4 = new Audio("audio/walk4.m4a")
+const treecheck = document.getElementById("treecheck")
+const birdcheck = document.getElementById("birdcheck")
+const flowercheck = document.getElementById("flowercheck")
+const gravecheck = document.getElementById("gravecheck")
+const mooncheck = document.getElementById("mooncheck")
+const tombcheck = document.getElementById("tombcheck")
+const statuecheck = document.getElementById("statuecheck")
+var step1 = new Audio("audio/walk1.mp3")
+var step2 = new Audio("audio/walk2.mp3")
+var step3 = new Audio("audio/walk3.mp3")
+var step4 = new Audio("audio/walk4.mp3")
 let state ;
 let bird ;
 let statue ;
@@ -52,6 +59,7 @@ forward.addEventListener("click", function(){
         // console.log("we're getting somewhere now baby")
         view.innerHTML = `<img src="images/main/main1f.jpg">`
         localStorage.setItem("state", "m1f")
+        dialogue.innerHTML = `<p>Attendez, qu'est-ce que c'est à gauche ?</p>`
     }
     
     else if (state == "m1f") {
@@ -87,6 +95,7 @@ forward.addEventListener("click", function(){
         // console.log("m7f")
         view.innerHTML = `<img src="images/main/main7f.jpg">`
         localStorage.setItem("state", "m7f")
+        dialogue.innerHTML = `<p>je pourrais prendre une photo de la lune d'ici...</p>`
     }
     else if (state == "m7f") {
         console.log("m7f bump")
@@ -95,6 +104,7 @@ forward.addEventListener("click", function(){
     else if (state == "m1l") {
         view.innerHTML = `<img src="images/main/bird1f.jpg">`
         localStorage.setItem("state", "b1f")
+        dialogue.innerHTML `<p> "Si je l'aligne correctement... ça pourrait être parfait."</p>`
     }
     // main
     // tomb
@@ -111,11 +121,13 @@ forward.addEventListener("click", function(){
     else if (state == "t3f") {
         view.innerHTML = `<img src="images/tomb/tomb4f.jpg">`
         localStorage.setItem ("state", "t4f")
+        dialogue.innerHTML = `<p>le chemin à droite pourrait mener à quelque chose d'intéressant</p>`
     }
 
     else if (state == "t4f") {
         view.innerHTML = `<img src="images/tomb/tomb5f.jpg">`
         localStorage.setItem ("state", "t5f")
+        dialogue.innerHTML = `<p>Hmmm. La façon dont ces pierres sont alignées.</p>`
     }
 
     else if (state == "t5f") {
@@ -126,10 +138,49 @@ forward.addEventListener("click", function(){
     else if (state == "t6f") {
         view.innerHTML = `<img src="images/tomb/tomb7f.jpg">`
         localStorage.setItem ("state", "t7f")
+        dialogue.innerHTML = `<p>Hmm, ça a l'air intéressant... Et il y a un arbre sympa à gauche...</p>`
     }
 
     else if (state == "t7f") {
         console.log (state + "bump")
+    }
+
+        else if (state == "t1b") {
+            console.log (state + "bump")
+
+    }
+
+    else if (state == "t2b") {
+        view.innerHTML = `<img src="images/tomb/tomb1b.jpg">`
+        localStorage.setItem ("state", "t1b")
+    }
+
+    else if (state == "t3b") {
+        view.innerHTML = `<img src="images/tomb/tomb2b.jpg">`
+        localStorage.setItem ("state", "t2b")
+        dialogue.innerHTML = `<p>le chemin à droite pourrait mener à quelque chose d'intéressant</p>`
+    }
+
+    else if (state == "t4b") {
+        view.innerHTML = `<img src="images/tomb/tomb3b.jpg">`
+        localStorage.setItem ("state", "t3b")
+        dialogue.innerHTML = `<p>Hmmm. La façon dont ces pierres sont alignées.</p>`
+    }
+
+    else if (state == "t5b") {
+        view.innerHTML = `<img src="images/tomb/tomb4b.jpg">`
+        localStorage.setItem ("state", "t4b")
+    }
+
+    else if (state == "t6b") {
+        view.innerHTML = `<img src="images/tomb/tomb5b.jpg">`
+        localStorage.setItem ("state", "t5b")
+        dialogue.innerHTML = `<p>Hmm, ça a l'air intéressant... Et il y a un arbre sympa à gauche...</p>`
+    }
+
+    else if (state == "t7b") {
+        view.innerHTML = `<img src="images/tomb/tomb6b.jpg">`
+        localStorage.setItem ("state", "t6b")
     }
     // tomb
     // statue
@@ -156,6 +207,7 @@ forward.addEventListener("click", function(){
         else if (state == "s4f") {
         view.innerHTML = `<img src="images/statue/statue5f.jpg">`
         localStorage.setItem("state", "s5f")
+        dialogue.innerHTML `<p>Attendez... Qu'est-ce que c'est là-bas, à gauche ?</p>`
     }
 
         else if (state == "s5f") {
@@ -171,6 +223,7 @@ forward.addEventListener("click", function(){
         else if (state == "s7f") {
         view.innerHTML = `<img src="images/statue/statue8f.jpg">`
         localStorage.setItem("state", "s8f")
+        dialogue.innerHTML `<p>Waouh, ce truc fait peur vu de près !</p>`
     }
 
         else if (state == "s8f") {
@@ -185,6 +238,7 @@ forward.addEventListener("click", function(){
         else if (state == "sub1f") {
             view.innerHTML = `<img src="images/sub/sub2f.jpg">`
             localStorage.setItem("state", "sub2f")
+            dialogue.innerHTML = `<p>J'espère avoir trouvé un bon angle pour photographier cet arbre.</p>`
         }
 
         else if (state == "sub2f") {
@@ -211,6 +265,32 @@ forward.addEventListener("click", function(){
             view.innerHTML = `<img src="images/russia/benchf1.jpg">`
             localStorage.setItem("state", "be1f")
         }
+
+                else if (state == "sub1b") {
+            view.innerHTML = `<img src="images/sub/sub2b.jpg">`
+            localStorage.setItem("state", "sub2b")
+            dialogue.innerHTML = `<p>J'espère avoir trouvé un bon angle pour photographier cet arbre.</p>`
+        }
+
+        else if (state == "sub2b") {
+            view.innerHTML = `<img src="images/sub/sub3b.jpg">`
+            localStorage.setItem("state", "sub3b")
+        }
+
+        else if (state == "sub3b") {
+            view.innerHTML = `<img src="images/sub/sub4b.jpg">`
+            localStorage.setItem("state", "sub4b")
+        }
+
+        else if (state == "sub4b") {
+            view.innerHTML = `<img src="images/sub/sub5b.jpg">`
+            localStorage.setItem("state", "sub5b")
+        }
+
+        else if (state == "sub5b") {
+            view.innerHTML = `<img src="images/sub/russia5l.jpg">`
+            localStorage.setItem("state", "r5b")
+        }
     // sub
     // russia
 
@@ -229,15 +309,39 @@ forward.addEventListener("click", function(){
         else if (state == "r4f") {
         view.innerHTML = `<img src="images/russia/russia5f.jpg">`
         localStorage.setItem("state. r5f")
+        dialogue.innerHTML = `<p>huh, i think i see a bench to the right"</p>`
     }
 
         else if (state == "r5f") {
             console.log (state + "bump")
     }
+
+    else if (state == "r1b") {
+        console.log (state + "bump")
+    }
+    else if (state == "r2b") {
+        view.innerHTML = `<img src="images/russia/russia1f.jpg">`
+        localStorage.setItem("state. r1b")
+    }
+    else if (state == "r3b") {
+        view.innerHTML = `<img src="images/russia/russia2f.jpg">`
+        localStorage.setItem("state. r2b")
+    }
+    else if (state == "r4b") {
+        view.innerHTML = `<img src="images/russia/russia3f.jpg">`
+        localStorage.setItem("state. r3b")
+    }
+    
+    else if (state == "r5b") {
+            view.innerHTML = `<img src="images/russia/russia4b.jpg">`
+            localStorage.setItem("state. r4b")
+    }
     
     else {
         console.log ("nomove" + state)
     }
+
+    step1.play ();
     state = localStorage.getItem("state");
     console.log(state)
 });
@@ -268,7 +372,11 @@ back.addEventListener("click", function(){
 
     else if (state == ("t7l")) {
         view.innerHTML = `<img src="images/tomb/tomb7f.jpg">`
-        localStorage.setItem("state", "t4f")
+        localStorage.setItem("state", "t7f")
+    }
+
+    else if (state == ("t7f")) {
+        view.innerHTML = `<img src="images/tomb/tomb7f.jpg">`
     }
 
     else if (state == ("sub1f")) {
@@ -284,6 +392,8 @@ back.addEventListener("click", function(){
     else {
 
     }
+
+    step4.play();
 
 });
     
@@ -323,11 +433,23 @@ left.addEventListener("click", function(){
         localStorage.setItem("state", "r1f")
     }
 
+    // statue
+    // russia
+    else if (state == "r3f") {
+        view.innerHTML = `<img src="images/russia/russia3l.jpg">`
+    }
+
+    else if (state == "r5f") {
+        view.innerHTML = `<img src="images/sub/russia5l.jpg">`
+        localStorage.setItem("state", "su5b")
+    }
+
     else {
-        console.log ("something")
+        console.log ("can't move")
     }
         state = localStorage.getItem("state");
     console.log(state)
+    step3.play();
     });
 
 
@@ -358,6 +480,15 @@ right.addEventListener("click", function(){
         localStorage.setItem("state", "sub1f")
     }
     // tomb
+
+    else if (state == "r3b") {
+        view.innerHTML = `<img src="images/russia/russia3l.jpg">`
+    }
+
+    else if (state == "su1b") {
+        view.innerHTML = `<img src="images/tomb7b.jpg">`
+        localStorage.setItem("state", "t7b")
+    }
     
     else {
         console.log ("nomove" + state)
@@ -365,6 +496,70 @@ right.addEventListener("click", function(){
     // view.innerHTML = `<img src="images/">`
     state = localStorage.getItem("state");
     console.log(state)
+    step2.play();
+});
+
+treecheck.addEventListener("click",function() {
+    tree1 = localStorage.getItem("treecheck")
+    tree2 = localStorage.getItem("treecheck")
+    if (tree1 == "1") {
+        view.innerHTML = (`<img src=images/digicam/tree1.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+
+    else if (tree2 == "1") {
+        view.innerHTML = (`<img src=images/digicam/tree2.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+
+    else if (tree1 == "1" + "tree2" == "2") {
+        view.innerHTML = (`<img src=images/digicam/tree2.png>`)
+        view.innerHTML = (`<img src=images/digicam/tree2.png>`)
+    }
+});
+flowercheck.addEventListener("click",function() {
+    flowers = localStorage.getItem("flower")
+    if (flowers == "1") {
+        view.innerHTML = (`<img src=images/digicam/flower.png`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+});
+birdcheck.addEventListener("click", function() {
+    bird = localStorage.getItem("bird")
+    if (bird == "1") {
+        view.innerHTML = (`<img src=images/digicam/bird.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+        console.log (`get fucked lmao`)
+    }
+    console.log (`click`)
+});
+gravecheck.addEventListener("click",function() {
+    graves = localStorage.getItem("grave")
+    if (graves == "1") {
+        view.innerHTML = (`<img src=images/digicam/grave.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+});
+mooncheck.addEventListener("click",function() {
+    moon = localStorage.getItem("moon")
+    if (moon == "1") {
+        view.innerHTML = (`<img src=images/digicam/moon.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+});
+tombcheck.addEventListener("click",function() {
+    tomb = localStorage.getItem("tomb")
+    if (tomb == "1") {
+        view.innerHTML = (`<img src=images/digicam/tomb.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
+});
+statuecheck.addEventListener("click",function() {
+    statue = localStorage.getItem("statue")
+    if (statue == "1") {
+        view.innerHTML = (`<img src=images/digicam/statue.png>`)
+        dialogue.innerHTML = (`<p>???</p>`)
+    }
 });
 
 shudder.addEventListener("click", function(){
@@ -377,11 +572,13 @@ shudder.addEventListener("click", function(){
     else if (state == "bpic") {
         view.innerHTML = `<img src="images/main/photo_bird.jpg">`
         localStorage.setItem ("bird", "1")
+        birdcheck.innerHTML `<s><ul> oiseau</ul></s>`
     }
 
     else if (state == "s8f") {
         view.innerHTML = `<img src="images/statue/photo_statue/jpg">`
         localStorage.setItem ("statue", "1")
+        statuecheck.innerHTML `<s><ul id="gravecheck"> pierres tombales</ul></s>`
     }
 
     else if (state == "be1f") {
@@ -407,6 +604,7 @@ shudder.addEventListener("click", function(){
     else if (state == "t7l") {
         view.innerHTML = `<img src="images/tomb/photo_tree1.jpg">`
         localStorage.setItem ("tree1", "1")
+        treecheck.innerHTML `<s><ul id="treecheck"> arbres</ul></s>`
     }
 
     else if (state == "t7f") {
@@ -414,12 +612,18 @@ shudder.addEventListener("click", function(){
         localStorage.setItem ("tomb", "1")
     }
 
-    else if (state == "r3l") {
+    else if (state == "r3b") {
+        view.innerHTML = `<img src="images/russia/photo_tree2.jpg">`
+        localStorage.setItem ("tree2", "1")
+    }
+
+        else if (state == "r3f") {
         view.innerHTML = `<img src="images/russia/photo_tree2.jpg">`
         localStorage.setItem ("tree2", "1")
     }
     
     else {
         dialogue.innerHTML = "<p>il y a rien interresant ici</p>"
+        console.log (`nopic` + state)
     }
 });
